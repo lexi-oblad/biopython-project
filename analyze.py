94e530a369614477b985775a117c3a1c497f5c1d
Lexi Oblad <alexia.oblad@gmail.com>
2020-07-31 20:30:40 -0700

    made jupyter notebook for covid sequence analysis

--- analyze.py
+++ analyze.py
@@ -1,19 +1,21 @@
 from Bio import SeqIO
-sequences = [gen for gen in SeqIO.parse("dataset/genome.fna", "fasta")]
-covid_seq_record = sequences[0]
+sequence = [gen for gen in SeqIO.parse("dataset/genome.fna", "fasta")]
+covid_seq_record = sequence[0]
 #assert len(covid_seq_record.seq) % 3 == 0
+print("The genome of the SARS-CoV-2 virus has " +
+      str(len(sequence[0])) + " base pairs.")
 
 # remove last 2 characters to allow translate without warning: must be divible 3
 covid_seq_record.seq = covid_seq_record.seq[:-2]
-assert len(covid_seq_record.seq) % 3 == 0
+covid_seq = covid_seq_record.seq
+assert len(covid_seq) % 3 == 0
 
-print(f"Length: { len(covid_seq_record.seq) }")
-print(f"Original DNA: {covid_seq_record.seq[:100]}")
-print("transcribing mRNA")
-mRNA = sequences[0].seq.transcribe()
+print(f"Length: {len(covid_seq)}")
+print(f"First 100 bp of virus DNA: {covid_seq[:100]}")
+mRNA = covid_seq.transcribe()
 print(len(mRNA) % 3)
 print("Transcribed DNA to mRNA: " + mRNA[:99])
 protein = mRNA.translate()
 print("Amino Acid Sequence: " + protein[:33])
 proteins = protein.split("*")
-print(proteins[:50])
+print(proteins[:10])
